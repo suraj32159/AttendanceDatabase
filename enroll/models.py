860a1affb6830aca8637ad38cb8d8c192e1598f0
Ankan <majumdarankan7.com>
2021-12-07 01:42:24 +0530

    done

--- enroll/models.py
+++ enroll/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+import os
 #from .views import add_show
 # Create your models here.
+
+
+def file_path_dir(instance, file_name):
+    return instance.name+'/{1}'.format(instance.id, file_name)
+
 class User(models.Model):
     name = models.CharField(max_length=70)
     enrollment_no = models.IntegerField()
-    file_name = models.FileField(upload_to='')
+    file_name = models.FileField(upload_to=file_path_dir)
