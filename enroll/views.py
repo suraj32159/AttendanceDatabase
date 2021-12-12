d28a21e71cdbdfd79ab9aff5d91a4dff000a16a4
suraj32159 <suraj32159@gmail.com>
2021-12-13 01:11:59 +0530

    successMsg

--- enroll/views.py
+++ enroll/views.py
@@ -4,6 +4,7 @@ from .forms import StudentRegistration
 from django.core.files.storage import FileSystemStorage
 from .models import User
 from django.conf import settings
+from django.contrib import messages
 import os
 
 # Create your views here.
@@ -11,11 +12,12 @@ def add_show(request):
 	if request.method == 'POST':
 		for f in request.FILES.getlist('file_name'):
 			User(name = request.POST.get('name'),course=request.POST.get('course'),enrollment_no = request.POST.get('enrollment_no'),file_name=f).save()
+		messages.success(request, 'Form submission successful')
 	
 	# Find encodings
 	# folder call
 	# append in exist file if not exist create
-	
+
 	fm = StudentRegistration()
 	return render(request,'enroll/addandshow.html',{'form':fm})
 
