bf4273140703e1c2a29c6fd29476424e66adc465
Ankan <majumdarankan7@gmail.com>
2021-12-13 16:58:34 -0800

    KnownEncodings

--- enroll/views.py
+++ enroll/views.py
@@ -6,6 +6,12 @@ from .models import User
 from django.conf import settings
 from django.contrib import messages
 import os
+import sys
+import cv2
+import pickle
+import numpy as np
+from deepface import DeepFace
+from retinaface import RetinaFace
 
 # Create your views here.
 def add_show(request):
@@ -14,13 +20,38 @@ def add_show(request):
 			User(name = request.POST.get('name'),course=request.POST.get('course'),enrollment_no = request.POST.get('enrollment_no'),file_name=f).save()
 		messages.success(request, 'Form submission successful')
 	
+		#save_path = settings.MEDIA_ROOT +"/"+str(request.POST.get('course')) 
+		save_path="../media/"+str(request.POST.get('course'))+"/" +str(request.POST.get('name'))
+		findencodings(request.POST.get('name'),request.POST.get('course'))
+		return HttpResponse(save_path)
 	# Find encodings
-	# folder call
+	# folder call"
+	# findencodings(request.POST.get('name'),request.POST.get('course'))
 	# append in exist file if not exist create
 
 	fm = StudentRegistration()
 	return render(request,'enroll/addandshow.html',{'form':fm})
 
+
+def findencodings(Sname,course):
+	MODEL = 'Facenet'
+	known_faces = []
+	known_names = []
+	path=settings.MEDIA_ROOT+"/"+str(course)
+	for name in os.listdir(path):
+		myDict=[]
+		for imgName in os.listdir(path+"/"+name):
+			image = cv2.imread(f'{path}/{name}/{imgName}')
+			encoding = DeepFace.represent(image,model_name=MODEL,model=DeepFace.build_model(MODEL),enforce_detection=False,detector_backend='retinaface',align=True,normalization='base')
+			myDict.append(encoding)
+		known_faces.append(myDict)
+		known_names.append(name)
+
+		save_path = settings.MEDIA_ROOT+"/Encodings/"+str(course)
+		save_encods = open(save_path,"wb")
+		pickle.dump(known_faces,save_encods)
+		pickle.dump(known_names,save_encods)
+		save_encods.close()
 
 def success(request):
     return HttpResponse('successfully uploaded')
